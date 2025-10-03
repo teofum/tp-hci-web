8c8440e9b387f092171c1ffaf6db3b778ed9a359
lolo-pb <lpizzutobeltran@gmail.com>
2025-10-03 12:57:19 +0000

    initial things

---
+++ src/components/MyLists.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import listEntry from '../components/listEntry.vue'
+</script>
+
+<template>
+  <div >
+    <div class="main_container">
+        <h1>Mis listas</h1>
+        <div class="listas">
+            <!--todo esto seguro esta mal-->
+            <li>
+                <listEntry />
+            </li>
+        </div>
+        <!--nueva lista button-->
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .myLists {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/MisListasView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MyListsComponent from '../components/MyLists.vue'
+</script>
+
+<template>
+  <main>
+    <MyListsComponent />
+  </main>
+</template>
