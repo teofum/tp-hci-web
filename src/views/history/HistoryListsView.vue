dfafd9587c657e100e72b602ccaa3a3ad95da1e1
Sebalee <slee@itba.edu.ar>
2025-10-10 18:43:37 -0300

    feat: better history ui

--- src/views/history/HistoryListsView.vue
+++ src/views/history/HistoryListsView.vue
@@ -10,87 +10,83 @@ const { lists } = storeToRefs(store);
 const router = useRouter();
 
 const searchQuery = ref('');
-const orderBy = ref('name'); 
-const filterBy = ref('all')
+const orderBy = ref('name');
+const filterBy = ref('all');
 
 function redirectList(id: number) {
   router.push({ name: 'list-detail', params: { id } });
 }
 
 // const filteredLists = ; // TODO listas filtradas
-
-
 </script>
 
-
 <template>
-    <v-container max-width="800" class="container">
-      <div class="d-flex flex-row justify-space-between align-center w-100">
-        <h1 class="heading text-high-emphasis">Historial</h1>
-      </div>
+  <v-container max-width="800" class="container">
+    <div class="d-flex flex-row justify-space-between align-center w-100">
+      <h1 class="heading text-high-emphasis">Historial</h1>
+    </div>
 
-      <div class="ml_listas_header d-flex flex-column gap-4">
+    <div class="d-flex flex-column ga-2 my-4">
+      <v-text-field
+        label="Buscar listas"
+        type="text"
+        class="w-100"
+        clearable
+        clear-icon="mdi-close-circle-outline"
+      />
+    </div>
 
-        <v-text-field
-          label="Buscar listas"
-        ></v-text-field>
+    <div class="d-flex gap-4">
+      <v-select :items="['Fecha', 'Items']" label="Ordenar por"></v-select>
 
-        <div class="d-flex gap-4">
-          <v-select
-            :items="['Fecha', 'Items']"
-            label="Ordenar por"
-          ></v-select>
+      <v-select
+        :items="['Hoy', 'Hace una semana', 'Hace un mes']"
+        label="Filtrar"
+      ></v-select>
+    </div>
 
-          <v-select
-            :items="['Hoy', 'Hace una semana', 'Hace un mes']"
-            label="Filtrar"
-          ></v-select>
-        </div>
-      </div>
+    <div class="ml_listas_container">
+      <ul class="ml_listas">
+        <ListItem
+          v-for="list in lists"
+          :key="list.id"
+          :name="list.name"
+          :emoji="list.emoji"
+          :detail="'TODO items completos'"
+          @click="redirectList(list.id)"
+          style="cursor: pointer"
+        >
+          <v-menu>
+            <template v-slot:activator="{ props: activatorProps }">
+              <v-btn
+                v-bind="activatorProps"
+                variant="text"
+                icon="mdi-dots-vertical"
+              />
+            </template>
 
-      <div class="ml_listas_container">
-        <ul class="ml_listas">
-          <ListItem
-            v-for="list in lists"
-            :key="list.id"
-            :name="list.name"
-            :emoji="list.emoji"
-            :detail="'TODO items completos'"
-            @click="redirectList(list.id)"
-            style="cursor: pointer;"
-          >
-            <v-menu>
-              <template v-slot:activator="{ props: activatorProps }">
-                <v-btn
-                  v-bind="activatorProps"
-                  variant="text"
-                  icon="mdi-dots-vertical"
-                />
-              </template>
-
-              <v-list>
-                <AddListDialog :list="list">
-                  <template v-slot:activator="{ props: activatorProps }">
-                    <v-list-item
-                      v-bind="activatorProps"
-                      prepend-icon="mdi-pencil-outline"
-                      title="Modificar"
-                    />
-                  </template>
-                </AddListDialog>
-                <v-list-item
-                  class="text-red"
-                  prepend-icon="mdi-delete-outline"
-                  title="Eliminar"
-                  @click="store.deleteList(list.id)"
-                />
+            <v-list>
+              <AddListDialog :list="list">
+                <template v-slot:activator="{ props: activatorProps }">
+                  <v-list-item
+                    v-bind="activatorProps"
+                    prepend-icon="mdi-pencil-outline"
+                    title="Modificar"
+                  />
+                </template>
+              </AddListDialog>
+              <v-list-item
+                prepend-icon="mdi-reload"
+                title="Recuperar"
+                @click="store.deleteList(list.id)"
+              />
             </v-list>
-            </v-menu>
-          </ListItem>
-        </ul>
-      </div>
+          </v-menu>
+        </ListItem>
+      </ul>
+    </div>
 
-      <AddListDialog>
+    <AddListDialog>
       <template v-slot:activator="{ props: activatorProps }">
         <v-fab
           v-bind="activatorProps"
@@ -103,9 +99,8 @@ function redirectList(id: number) {
         />
       </template>
     </AddListDialog>
-    </v-container>
+  </v-container>
 </template>
-
 
 <style scoped>
 .heading {
@@ -113,4 +108,3 @@ function redirectList(id: number) {
   font-weight: 700;
 }
 </style>
-
