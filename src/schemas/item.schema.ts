83a7464c1df788650ab38880dcb2bb35288b7994
lolo-pb <lpizzutobeltran@gmail.com>
2025-10-10 21:05:15 +0000

    get items

---
+++ src/schemas/item.schema.ts
@@ -0,0 +1,25 @@
+import z from 'zod';
+
+import { dateSchema } from './date.schema';
+import { productSchema } from './product.schema';
+
+export const itemSchema = z
+  .object({
+    id: z.int(),
+    quantity: z.int(),
+    unit: z.string(),
+    metadata: z.object({
+      emoji: z.emoji(),
+    }),
+    purchased: z.boolean(),
+    lastPurchasedAt: dateSchema,
+    createdAt: dateSchema,
+    updatedAt: dateSchema,
+    product: productSchema,
+  })
+  .transform(({ metadata, ...res }) => ({
+    ...metadata,
+    ...res,
+  }));
+
+export type Item = z.infer<typeof itemSchema>;
